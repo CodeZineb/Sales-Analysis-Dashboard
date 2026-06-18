0c040eee1afbfdd1550e5a592da8c2a24f2a6bb5
CodeZineb <zinebboussihi@gmail.com>
2026-06-18 12:46:51 +0100

    Add SQL query to FactInternetSales.sql

---
+++ data_model /FactInternetSales.sql
@@ -0,0 +1,30 @@
+SELECT [ProductKey],
+      [OrderDateKey],
+      [DueDateKey],
+      [ShipDateKey],
+      [CustomerKey],
+      --,[PromotionKey]
+      --,[CurrencyKey]
+      --,[SalesTerritoryKey]
+      [SalesOrderNumber],
+      --,[SalesOrderLineNumber]
+      --,[RevisionNumber]
+      --,[OrderQuantity]
+      --,[UnitPrice]
+      --,[ExtendedAmount]
+      --,[UnitPriceDiscountPct]
+      --,[DiscountAmount]
+      --,[ProductStandardCost]
+      --,[TotalProductCost]
+      [SalesAmount]
+      --,[TaxAmt]
+      --,[Freight]
+      --,[CarrierTrackingNumber]
+      --,[CustomerPONumber]
+      --,[OrderDate]
+      --,[DueDate]
+      --,[ShipDate]
+  FROM [dbo].[FactInternetSales]
+  WHERE 
+  LEFT (OrderDateKey,4) >= YEAR(GetDate())-2
+  ORDER BY OrderDateKey ASC
